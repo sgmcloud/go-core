e787d038ddcc01d96b0d90db31f0581b999ecc49
Leo Moutinho <sgmcloud.app@gmail.com>
2023-01-21 19:06:49 -0300

    added: error field in logger.error function

--- log/logger.go
+++ log/logger.go
@@ -4,7 +4,7 @@ type Logger interface {
 	Debug(msg string, fields ...Field)
 	Info(msg string, fields ...Field)
 	Warn(msg string, fields ...Field)
-	Error(msg string, fields ...Field)
+	Error(msg string, err error, fields ...Field)
 	Fatal(msg string, fields ...Field)
 }
 
